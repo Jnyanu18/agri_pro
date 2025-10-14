70fb675398f9cef23a4c867266676956d52ce743
Mohammed Nooh <mdnooh49@gmail.com>
2025-10-14 18:54:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4063063908-148ad",
+  "appId": "1:276702333801:web:4422d5eb3aa06205e98bf8",
+  "apiKey": "AIzaSyAOwQGD4jma-eZGVCvsGfEV3rWyeIuUNVE",
+  "authDomain": "studio-4063063908-148ad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "276702333801"
+};
