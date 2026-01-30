8d72fd37b3a2e9c65ac9689084d952919465f849
Mohammed Nooh <mdnooh49@gmail.com>
2026-01-30 19:53:43 +0000

    correct all errors

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4063063908-148ad",
-  "appId": "1:276702333801:web:4422d5eb3aa06205e98bf8",
-  "apiKey": "AIzaSyAOwQGD4jma-eZGVCvsGfEV3rWyeIuUNVE",
-  "authDomain": "studio-4063063908-148ad.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "276702333801"
+  apiKey: "demo",
+  authDomain: "demo.firebaseapp.com",
+  projectId: "demo",
+  storageBucket: "demo.appspot.com",
+  messagingSenderId: "000000",
+  appId: "1:000000:web:demo"
 };
